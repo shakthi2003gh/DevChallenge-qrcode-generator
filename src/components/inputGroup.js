1398e15e4fa10fd83889fcaf7cad6608b93cb0c4
shakthi <shakthikumar123456789@gmail.com>
2023-10-02 16:34:52 +0530

    Create inputgroup component

---
+++ src/components/inputGroup.js
@@ -0,0 +1,20 @@
+const inputGroup = document.createElement("div");
+const input = document.createElement("input");
+const button = document.createElement("button");
+
+input.type = "text";
+input.placeholder = "Enter an url";
+input.oninput = () => {
+  const text = input.value.trim();
+  if (text.length === 0) button.disabled = true;
+  else button.disabled = false;
+};
+
+button.className = "btn btn--primary";
+button.textContent = "QR code";
+button.disabled = true;
+
+inputGroup.className = "input-group";
+inputGroup.append(input, button);
+
+export default inputGroup;
