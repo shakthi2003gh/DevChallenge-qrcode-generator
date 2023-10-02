76604ed745daedd6f538239a1300c07d2c5bc37b
shakthi <shakthikumar123456789@gmail.com>
2023-10-02 16:28:38 +0530

    Create logo component

---
+++ src/components/logo.js
@@ -0,0 +1,8 @@
+import logo from "./../assets/logo-qr-generator.svg";
+
+const img = document.createElement("img");
+img.classList = "logo";
+img.src = logo;
+img.alt = "QR CODE";
+
+export default img;
